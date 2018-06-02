7fe41e34e0e47a11ba2fe88d00898b86d1d13e1d
Timothy <yexiaozhou2003@gmail.com>
2018-06-03 00:59:50 +0800

    add query method

--- rate.go
+++ rate.go
@@ -1,1 +1,13 @@
 package core
+
+func Query(from string, to []string, amount float32) {
+	helper := NewRateHelper()
+	helper.FromCurrency = from
+	helper.ToCurrency = to
+	helper.Amount = amount
+
+	// Start query
+	helper.Query()
+
+	// Parse result
+}
