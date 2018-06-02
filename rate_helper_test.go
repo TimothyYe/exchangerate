0f2a7bc6e171e5a9252a2a8ab1eea4dd2f702a1f
Timothy <yexiaozhou2003@gmail.com>
2018-06-02 13:48:19 +0800

    update test case

--- rate_helper_test.go
+++ rate_helper_test.go
@@ -1,6 +1,7 @@
 package core
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -26,5 +27,31 @@ func TestQuery(t *testing.T) {
 
 	if len(helper.Result) != 3 {
 		t.Fatal("Query result is not correct...")
+	}
+
+	for k, v := range helper.Result {
+		fmt.Printf("%s %s\r\n", k, v)
+	}
+}
+
+func TestQueryWithAmount(t *testing.T) {
+	helper := NewRateHelper()
+
+	if helper == nil {
+		t.Fatal("New rate helper failed...")
+		return
+	}
+
+	helper.FromCurrency = "USD"
+	helper.Amount = 2
+	helper.ToCurrency = []string{"CNY", "JPY", "AUD"}
+	helper.Query()
+
+	if len(helper.Result) != 3 {
+		t.Fatal("Query result is not correct...")
+	}
+
+	for k, v := range helper.Result {
+		fmt.Printf("%s %s\r\n", k, v)
 	}
 }
